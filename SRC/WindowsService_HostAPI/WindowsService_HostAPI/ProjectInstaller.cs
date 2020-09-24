f9ef74a762ca0821b075b2b631754f69d5b66930
GlenDudley-E10150 <gdudley@aflac.com>
2020-09-23 21:40:38 -0400

    Updated Installers

--- SRC/WindowsService_HostAPI/WindowsService_HostAPI/ProjectInstaller.cs
+++ SRC/WindowsService_HostAPI/WindowsService_HostAPI/ProjectInstaller.cs
@@ -15,5 +15,10 @@ namespace WindowsService_HostAPI
         {
             InitializeComponent();
         }
+
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
     }
 }
